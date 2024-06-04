d0472079fa35a89d39f7f4464e2ee2e448658380
axdeux <tobiasmyrmel@hotmail.com>
2024-06-04 14:21:25 +0200

    Changed model for image generation

--- src/image_model.py
+++ src/image_model.py
@@ -1,35 +1,17 @@
 import torch
-from diffusers import AutoPipelineForText2Image
+from diffusers import StableDiffusionXLPipeline
 
 
 def generate_image(visual_prompt):
-    """Takes a text decsription and returns an image using the wuerstchen model.
-
-    Args:
-        visual_prompt (str): The text description of the image to be generated.
-
-    Returns:
-        PIL Image: The generated image.
-    
-    """
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    pipeline =  AutoPipelineForText2Image.from_pretrained(
-        "warp-diffusion/wuerstchen"
-    ).to(device)
+
+    pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, added_cond_kwargs={})
+    pipe = pipe.to(device)
+
+    
+    image = pipe(visual_prompt, num_inference_steps=20).images[0]
+
+    return image
 
 
-    description = "Pokemon 3d animation style. Entire body and realistic detailed. " + visual_prompt
-    negative_prompt = "portrait, HD, drawing, painting, pixel art, text"
-
-    output = pipeline(
-        prompt=description,
-        height=256,
-        width=256,
-        prior_guidance_scale=4.0,
-        decoder_guidance_scale=0.0,
-        negative_prompt=negative_prompt,
-    ).images
-
-    return output[0]
